b0040b23f8675be5db54314d2649625d1035ca60
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2023-04-09 02:11:44 -0300

    Update Questão 9.java

--- Questão 9.java
+++ Questão 9.java
@@ -1,61 +1,33 @@
-import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Questão9 {
-/* 9) Faça um programa para gerar todas as permutações de um conjunto de
-caracteres (por exemplo, "ABC" pode gerar "ABC", "ACB", "BAC",
-"BCA", "CAB" e "CBA"). */
-	public static void main(String[] args) {
-		System.out.println("Escreva seu conjunto de caracteres:");
-		Scanner leitor = new Scanner(System.in);
-		int i;
-		int numero= 3;
-		ArrayList<Character> lista = new ArrayList<Character>();
-		for( i=0;i<numero;i++) {
-			System.out.println("Escreva o caractere "+ i + ": ");
-			lista.add(leitor.next().charAt(0));
-		}
-		
-		for(int j=0;j<lista.size();j++) {
-			System.out.print(lista.get(j));       
-		}
-		char u;
-		System.out.println("Permutações possíveis:");
-		for(int j=0;j<lista.size()*2;j++) {
-			for(int k=0;j<lista.size();k++) {
-				System.out.print(lista.get(k));
-			}
-			u=lista.get(0);
-			
-		}
-		leitor.close();
-	}
+	/* 9) Faça um programa para gerar todas as permutações de um conjunto de
+	caracteres (por exemplo, "ABC" pode gerar "ABC", "ACB", "BAC",
+	"BCA", "CAB" e "CBA"). */
+	   public static void main(String[] args) {// precisei de ajuda do chat gpt mas entendi o código
+		   Scanner leitor = new Scanner(System.in);
+		   System.out.println("Escreva um Conjunto de 3 caracteres: ");
+	       String conjunto = leitor.next();
+	       gerarPermutações(conjunto.toCharArray(), 0);
+	       leitor.close();
+	   }
 
-}
-import java.util.Arrays;
+	   private static void gerarPermutações(char[] conjunto, int index) {
+	       if (index == conjunto.length - 1) {
+	    	   System.out.println(String.valueOf(conjunto));//devolve o array de char como string
+	       } 
+	       else {
+	    	   for (int i = index; i < conjunto.length; i++) {
+	             swap(conjunto, index, i);
+	             gerarPermutações(conjunto, index + 1);
+	             swap(conjunto, index, i);
+	           }
+	       }
+	   }
 
-public class PermutationGenerator {
-    
-    public static void main(String[] args) {
-        String input = "ABC";
-        generatePermutations(input.toCharArray(), 0);
-    }
-
-    private static void generatePermutations(char[] arr, int index) {
-        if (index == arr.length - 1) {
-            System.out.println(String.valueOf(arr));
-        } else {
-            for (int i = index; i < arr.length; i++) {
-                swap(arr, index, i);
-                generatePermutations(arr, index + 1);
-                swap(arr, index, i);
-            }
-        }
-    }
-
-    private static void swap(char[] arr, int i, int j) {
-        char temp = arr[i];
-        arr[i] = arr[j];
-        arr[j] = temp;
-    }
+	   private static void swap(char[] conjunto, int i, int j) { 
+	       char temp = conjunto[i];
+	       conjunto[i] = conjunto[j];
+	       conjunto[j] = temp;
+	   }
 }
