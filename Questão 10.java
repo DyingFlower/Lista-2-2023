d7c8a755d494b0fd59310b0f4f9c61f44d89cee8
DyingFlower <112014310+DyingFlower@users.noreply.github.com>
2023-04-11 16:50:47 -0300

    Update Questão 10.java

--- Questão 10.java
+++ Questão 10.java
@@ -23,19 +23,19 @@ a)              b)            C)          D)            E)
 		System.out.println("Diga qual Configuração de você quer: ");
 		String config = leitor.next(); // não sei ler char
 		leitor.close();
-		if(config.equals("a")|| config.equals("A")) {
+		if(config.equals("a")|| config.equals("A")) { // perfeito
 			configa(cl);
 		}
-		if(config.equals("b")|| config.equals("B")) {
+		if(config.equals("b")|| config.equals("B")) {// funciona legal só com 5 linhas e colunas
 			configb(cl);
 		}
-		if(config.equals("c")|| config.equals("C")) {
+		if(config.equals("c")|| config.equals("C")) {// perfeito
 			configc(cl);
 		}
-		if(config.equals("d")|| config.equals("D")) {
+		if(config.equals("d")|| config.equals("D")) {// funciona melhor com numeros impares de colunas e linhas
 			configd(cl);
 		}
-		if(config.equals("e")|| config.equals("E")) {
+		if(config.equals("e")|| config.equals("E")) {// só funciona direito para o numero de colunas e linhas 5
 			confige(cl);
 		}
 	}
@@ -136,7 +136,88 @@ a)              b)            C)          D)            E)
 		}
 	}
 	public static void configd(int cl) {
+		ArrayList<String[]> vetores= new ArrayList<>(); // necessário vetor de STRING DEVIDO O ||
+		for(int i=0;i<cl;i++) {
+			String vetor[]= new String[cl];
+			for(int j=0;j<vetor.length;j++) {
+				if(i<=cl/2||i==cl/2+0.5) {
+					if(j<cl/2) {
+						if(i==cl/2+0.5|| i==cl/2) {
+							vetor[j]="=";
+						}
+						else
+							vetor[j]="#";
+					}
+						else if(j>cl/2) {
+							if(i==cl/2+0.5 || i==cl/2) {
+								vetor[j]="=";
+							}
+							else
+								vetor[j]="!";
+						}
+						else if(j==cl/2+0.5 || j==cl/2) {
+							vetor[j]="||";
+						}	
+				}
+				else if(i>cl/2) {
+					if(j<cl/2) {
+						vetor[j]="*";
+					}
+					else if(j>cl/2) {
+						vetor[j]="%";
+					}
+					else if(j==cl/2+0.5 || j==cl/2) {
+						vetor[j]="||";
+					}	
+				}
+			}
+			vetores.add(vetor);
+		}
+		for(int i=0;i<cl;i++) {
+			String vetor[] = vetores.get(i);
+			 for (int j = 0; j < vetor.length; j++) {
+				 System.out.print(vetor[j] + " ");
+	         }
+			 System.out.println();
+		}
 	}
 	public static void confige(int cl) {
+		ArrayList<int[]> vetores= new ArrayList<>();
+		for(int i=0;i<cl;i++) {
+			int vetor[]= new int[cl];
+			for(int j=0;j<vetor.length;j++) {
+				vetor[j]=j;
+			}
+			vetores.add(vetor);
+		}
+		for(int i=0;i<cl;i++) {
+			int vetor[] = vetores.get(i);
+			 for (int j = 0; j < vetor.length; j++) {
+				 if(i==0 || i== cl-1) {
+					 if(j!=0 && j!=cl-1) {
+						 System.out.print("* ");
+					 }
+					 else
+						 System.out.print(vetor[j] + " ");
+				 }
+				 else if(i<cl/2 || i>cl/2) {
+					 if(j==0 || j==cl-1 || j==cl/2 || j==cl/2+0.5) {
+						 System.out.print("* ");
+					 }
+					 else
+						 System.out.print(vetor[j] + " ");
+				 }
+				 else if(i==cl/2+0.5 || i==cl/2|| i==cl/2+1) {
+					 if(j==cl/2+0.5 ||j==cl/2) {
+						 System.out.print(vetor[j] + " "); 
+					 }
+					 else {
+						 System.out.print("* "); 
+					 }
+				 }
+				
+	         }
+			 System.out.println();
+		}
 	}
 }
